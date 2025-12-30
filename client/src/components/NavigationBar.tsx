08df78adf0337cc9c853950856d869eda69e51f3
Esther Ng <esther.ng@outlook.my>
2025-12-30 13:30:55 +0800

    fixed nav bar not collapse afer navigation

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -1,20 +1,45 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-const NavigationBar = () => (
-  <Navbar bg="dark" variant="dark" expand="lg" fixed="top" className="w-100">
-    <Container>
-      <Navbar.Brand as={Link} to="/">Esther Ng</Navbar.Brand>
-      <Navbar.Toggle aria-controls="main-navbar" />
-      <Navbar.Collapse id="main-navbar">
-        <Nav className="ms-auto">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/cv">About Me</Nav.Link>
-          <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+const NavigationBar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavClick = () => setExpanded(false);
+
+  return (
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="lg"
+      fixed="top"
+      className="w-100"
+      expanded={expanded}
+    >
+      <Container>
+        <Navbar.Brand as={Link} to="/" onClick={handleNavClick}>
+          Esther Ng
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="main-navbar"
+          onClick={() => setExpanded(expanded ? false : true)}
+        />
+        <Navbar.Collapse id="main-navbar">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/" onClick={handleNavClick}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/cv" onClick={handleNavClick}>
+              About Me
+            </Nav.Link>
+            <Nav.Link as={Link} to="/projects" onClick={handleNavClick}>
+              Projects
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavigationBar;
